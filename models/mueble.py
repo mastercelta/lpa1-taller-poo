b6f910dc22c0adf778d6a1d84782c0871e7e7142
mastercelta <mastercelta@users.noreply.github.com>
2026-09-22 02:14:00 +0000

    implementar __str__ de Mueble

--- models/mueble.py
+++ models/mueble.py
@@ -86,9 +86,7 @@ class Mueble(ABC):
         Representación en cadena del mueble.
         Este método concreto puede ser usado por todas las clases hijas.
         """
-        # TODO: Implementar usando las propiedades
-        # return f"{self.nombre} de {self.material} en color {self.color}"
-        pass
+        return f"{self.nombre} de {self.material} en color {self.color}"
     
     def __repr__(self) -> str:
         """
