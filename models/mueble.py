eca4d7c18d7bec71667e85f5cafe3e2339de6272
mastercelta <mastercelta@users.noreply.github.com>
2026-09-22 02:09:39 +0000

    agregar getters de Mueble

--- models/mueble.py
+++ models/mueble.py
@@ -33,14 +33,22 @@ class Mueble(ABC):
         self._color = color
         self._precio_base = precio_base
     
-    # TODO: Implementar las propiedades (getters) para cada atributo
-    # Usa el decorador @property para crear getters
-    # Ejemplo:
-    # @property
-    # def nombre(self) -> str:
-    #     """Getter para el nombre del mueble."""
-    #     return self._nombre
-    
+    @property
+    def nombre(self) -> str:
+        return self._nombre
+
+    @property
+    def material(self) -> str:
+        return self._material
+
+    @property
+    def color(self) -> str:
+        return self._color
+
+    @property
+    def precio_base(self) -> float:
+        return self._precio_base
+
     # TODO: Implementar los setters para cada atributo donde sea necesario
     # Usa el decorador @nombre.setter para crear setters
     # Incluye validaciones básicas (ej: precio no puede ser negativo)
